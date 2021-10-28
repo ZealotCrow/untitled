3890a89648f9abbe03fbb521242468b23e617a42
True Zealot <61333869+ZealotCrow@users.noreply.github.com>
2021-10-27 22:56:06 -0400

    Fuck Yeah

--- src/com/company/characterInfo.java
+++ src/com/company/characterInfo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 //You've just encountered a new ally. Please provide their information
 public class characterInfo {
     private String name;
-    private String gender;
+    private static String gender;
     private String bodyType;
 
     public void setName(String nam) {
@@ -12,6 +12,7 @@ public class characterInfo {
 
     public static String getName() {
         Scanner characterName = new Scanner(System.in);
+        System.out.println("Enter the name of your valiant savior!");
         String name = characterName.nextLine();
         return name;
     }
@@ -20,13 +21,21 @@ public class characterInfo {
         this.gender = gender;
     }
 
-    public String getGender() {
+    public static String getGender() {
         String weapon;
         String description;
         String[] characterClass = {"Paladin","Knight","Demon Slayer","God","Grief Cleric"};
         for (int i=0; i < characterClass.length; i++)
             if (characterClass[0] == "Paladin"){
-
+                weapon = "Holy Blade";
+                description = "The bold paladin from the lands of Holy Terra stunts the growth of corruption where" +
+                        "it is found and dashes it with the glory of it's deity";
+                System.out.println(weapon + " " +description );
+            }
+            else if (characterClass[1] == "Knight"){
+                weapon = "Jaded Blade";
+                description ="The battle-strain and blood-stained knight of old dead-set on defending queen and country" +
+                        "from does that may seek out it's heart";
             }
         return gender;
     }
