108f0004c9d2199857d25fe2a64d59a9b4dfe4cc
True Zealot <61333869+ZealotCrow@users.noreply.github.com>
2021-10-30 18:38:32 -0400

    Fuck Yeah

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     };
     public static void main(String[] args) {
 	// write your code here
-        int tony = 19;
+        /*int tony = 19;
         if(tony == 19){
             System.out.println("Free Palistine");
         }
@@ -23,7 +23,8 @@ public class Main {
         System.out.println(freddie.equals(name));
         System.out.println("The sum of the equation is " + 959 / 20 +" and the modulus is " + 959 % 20);
         System.out.println(242+642+507+2967+498+209/6);
-        System.out.println(characterInfo.getName());
+        //System.out.println(characterInfo.getName());
+        */
         System.out.println(characterInfo.getCharacterSpecs());
 
 
--- src/com/company/characterInfo.java
+++ src/com/company/characterInfo.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
 //You've just encountered a new ally. Please provide their information
 public class characterInfo {
@@ -11,66 +12,73 @@ public class characterInfo {
         this.name = nam;
     }
 
-    public static String getName() {
+   /* public static String getName() {
         Scanner characterName = new Scanner(System.in);
         System.out.println("Enter the name of your valiant savior!");
         String name = characterName.nextLine();
         return name;
     }
+    */
+
 
     public void setCharacterSpecs(String characterSpecs) {
         this.characterSpecs = characterSpecs;
     }
 
-
+        //for (int i = 0; i < characterClass.length; )//Solve infinite issue
     public static String getCharacterSpecs() {
         String weapon;
         String weaponIntro = "The weapon that this hero specializes in is the: ";
         String description;
-        String[] characterClass = {"Paladin","Knight","Demon Slayer","God","Grief Cleric"};
 
-        for (int i = 0; i < characterClass.length; )//Solve infinite issue
+        String[] characterClass = {"Paladin", "Knight", "Demon Slayer", "God", "Grief Cleric"};
 
-            if (characterClass[0] == "Paladin"){
+
+
+
+        for (int i = 0; i < characterClass.length;i++) {
+
+            Scanner pickNum = characterClass[i];
+
+            if (characterClass[0] == "Paladin") {
                 weapon = "Holy Blade";
                 description = "The bold paladin from the lands of Holy Terra stunts the growth of corruption where" +
-                        "it is found and dashes it with the glory of it's deity";
+                        " it is found and dashes it with the glory of it's deity";
+                System.out.println(characterClass[0]);
                 System.out.println(weaponIntro + weapon);
                 System.out.println(description);
 
-            }
-            else if (characterClass[1] == "Knight"){
+            } else if (characterClass[1] == "Knight") {
                 weapon = "Jaded Blade";
-                description ="The battle-strain and blood-stained knight of old dead-set on defending queen and country" +
-                        "from does that may seek out it's heart";
+                description = "The battle-strain and blood-stained knight of old dead-set on defending queen and country" +
+                        " from does that may seek out it's heart";
                 System.out.println(weaponIntro + weapon);
                 System.out.println(description);
-            }
-            else if(characterClass[2] == "Demon Slayer"){
+            } else if (characterClass[2] == "Demon Slayer") {
                 weapon = "Rinamusashi, The Void Katana";
                 description = "The creeping chaos is vast and cold and there are those who need to be protected." +
-                        "That's where you come in. You are the looker and the lurker of the abyss";
+                        " That's where you come in. You are the looker and the lurker of the abyss";
                 System.out.println(weaponIntro + weapon);
                 System.out.println(description);
-            }
-            else if (characterClass[3] == "God"){
+            } else if (characterClass[3] == "God") {
                 weapon = "The Entropy Chalice";
                 description = "To call them a God in any mystical sense is incorrect. They have reached the ultimate human understanding of entropy and are able to grow from it" +
                         ", reaching the greatest possible path of their ilk";
-                System.out.println(weaponIntro +weapon);
+                System.out.println(weaponIntro + weapon);
                 System.out.println(description);
-            }
-            else if (characterClass[4] == "Grief Cleric"){
+            } else if (characterClass[4] == "Grief Cleric") {
                 weapon = "N/A";
                 description = "Not a fighter in a physical sense but a fighter for the beautiful and decent. Set to access the others in you charge to fight again";
                 System.out.println(weaponIntro + weapon);
                 System.out.println(description);
 
-            }
-            else    {
+            } else {
                 System.out.println("The heroes, while valiant and graceful, do not take kindly to having their time wasted. Journey on and continue you path!");
             }
-        return characterSpecs;
-    }
+            break;
+        }
+            return characterSpecs;
+        }
+
 
 }
